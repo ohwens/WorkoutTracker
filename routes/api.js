dd0921ecf437c59954029cf0c9161f246c62ef55
Ryan Owens <rtowens11@gmail.com>
2021-08-25 22:17:28 -0500

    other routes are working...i think

--- routes/api.js
+++ routes/api.js
@@ -3,14 +3,14 @@ const Workout = require("../models/workout");
 
 router.get("/workouts", (req, res) => {
     Workout.aggregate([
-      {
-        $addFields: {
-          totalDuration: {
-            $sum: "$exercises.duration"
-          } 
+        {
+          $addFields: {
+            totalDuration: {
+              $sum: "$exercises.duration"
+            } 
+          }
         }
-      }
-    ])
+      ])
     .then(dbWorkout => {
       res.json(dbWorkout);
     })
@@ -19,5 +19,46 @@ router.get("/workouts", (req, res) => {
     });
 
 })
+
+router.get("/workouts/range", (req, res) => {
+    Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: {
+            $sum: "$exercises.duration"
+          } 
+        }
+      }
+    ])
+    .sort({ date: -1 })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+})
+
+router.post("/workouts/", (req, res) => {
+    Workout.create({})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+})
+
+router.put("/workouts/:id", (req ,res) => {
+
+    Workout.findOneAndUpdate({_id: req.params.id }, { $push: { exercises: req.body } }, { new: true })
+
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
 
 module.exports = router;
