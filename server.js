4f781f024f9e9c71a3aa3fb3d48527fadd412607
Ryan Owens <rtowens11@gmail.com>
2021-08-25 21:29:09 -0500

    html routes work

--- server.js
+++ server.js
@@ -20,6 +20,18 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   }
 );
 
+// html routes
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
